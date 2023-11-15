5f6872a3ee6c6846b8da4c71f379d4034086c7c2
sarahwalenciak <143658772+sarahwalenciak@users.noreply.github.com>
2023-11-14 21:56:26 -0500

    first commit

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    public AudioClip collectedClip;
+    public ParticleSystem HealthEffect;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if(controller.health  < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Instantiate(HealthEffect, other.transform.position, Quaternion.identity).Play();
+                Destroy(gameObject);
+
+                controller.PlaySound(collectedClip);
+}
+        }
+
+    }
+}
